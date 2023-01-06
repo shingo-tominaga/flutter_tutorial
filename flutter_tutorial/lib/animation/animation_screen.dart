3a9710559b7c8c8b043406ca4268bf8fc139d135
shingo.tominaga <shingo.tominaga@axia-works.com>
2023-01-06 16:31:21 +0900

    tutorial1.1_AnimatedBuilderを使ったWidgetからAnimatedWidgetに戻した、点滅アニメーションを作成

--- flutter_tutorial/lib/animation/animation_screen.dart
+++ flutter_tutorial/lib/animation/animation_screen.dart
@@ -5,15 +5,21 @@ class AnimatedLogo extends AnimatedWidget {
   const AnimatedLogo({super.key, required Animation<double> animation})
       : super(listenable: animation);
 
+  static final _opacityTween = Tween<double>(begin: 0.1, end: 1);
+  static final _sizeTween = Tween<double>(begin: 0, end: 300);
+
   @override
   Widget build(BuildContext context) {
     final animation = listenable as Animation<double>;
     return Center(
-      child: Container(
-        margin: const EdgeInsets.symmetric(vertical: 10),
-        height: animation.value,
-        width: animation.value,
-        child: const FlutterLogo(),
+      child: Opacity(
+        opacity: _opacityTween.evaluate(animation),
+        child: Container(
+          margin: const EdgeInsets.symmetric(vertical: 10),
+          height: _sizeTween.evaluate(animation),
+          width: _sizeTween.evaluate(animation),
+          child: const FlutterLogo(),
+        ),
       ),
     );
   }
@@ -76,7 +82,8 @@ class _AnimationScreenState extends State<AnimationScreen>
     super.initState();
     controller =
         AnimationController(duration: const Duration(seconds: 2), vsync: this);
-    animation = Tween<double>(begin: 0, end: 300).animate(controller)
+    // animation = Tween<double>(begin: 0, end: 300).animate(controller)
+    animation = CurvedAnimation(parent: controller, curve: Curves.easeIn)
       ..addStatusListener((status) {
         if (status == AnimationStatus.completed) {
           controller.reverse();
@@ -89,10 +96,10 @@ class _AnimationScreenState extends State<AnimationScreen>
   }
 
   @override
-  // Widget build(BuildContext context) => AnimatedLogo(animation: animation);
-  Widget build(BuildContext context) {
-    return GrowTransition(animation: animation, child: const LogoWidget());
-  }
+  Widget build(BuildContext context) => AnimatedLogo(animation: animation);
+  // Widget build(BuildContext context) {
+  //   return GrowTransition(animation: animation, child: const LogoWidget());
+  // }
 
   @override
   void dispose() {
