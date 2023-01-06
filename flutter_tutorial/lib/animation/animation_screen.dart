71242daf2375003b7b930657544b65c75438380d
shingo.tominaga <shingo.tominaga@axia-works.com>
2023-01-06 14:21:25 +0900

    tutorial1.1_Flutterロゴが徐々に大きくなるアニメーション作成

--- flutter_tutorial/lib/animation/animation_screen.dart
+++ flutter_tutorial/lib/animation/animation_screen.dart
@@ -7,14 +7,30 @@ class AnimationScreen extends StatefulWidget {
   State<StatefulWidget> createState() => _AnimationScreenState();
 }
 
-class _AnimationScreenState extends State<AnimationScreen> {
+class _AnimationScreenState extends State<AnimationScreen>
+    with SingleTickerProviderStateMixin {
+  late Animation<double> animation;
+  late AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller =
+        AnimationController(duration: const Duration(seconds: 2), vsync: this);
+    animation = Tween<double>(begin: 0, end: 300).animate(controller)
+      ..addListener(() {
+        setState(() {});
+      });
+    controller.forward();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Container(
         margin: const EdgeInsets.symmetric(vertical: 10),
-        height: 300,
-        width: 300,
+        height: animation.value,
+        width: animation.value,
         child: const FlutterLogo(),
       ),
     );
