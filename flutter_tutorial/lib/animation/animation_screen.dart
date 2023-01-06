19a0300c591838223e3cf8ce9d1171732a9402a1
shingo.tominaga <shingo.tominaga@axia-works.com>
2023-01-06 14:38:36 +0900

    tutorial1.1_AnimatedWidget作成、アニメーション実装部分を作成したAnimatedLogoを使うように

--- flutter_tutorial/lib/animation/animation_screen.dart
+++ flutter_tutorial/lib/animation/animation_screen.dart
@@ -1,5 +1,23 @@
 import 'package:flutter/material.dart';
 
+class AnimatedLogo extends AnimatedWidget {
+  const AnimatedLogo({super.key, required Animation<double> animation})
+      : super(listenable: animation);
+
+  @override
+  Widget build(BuildContext context) {
+    final animation = listenable as Animation<double>;
+    return Center(
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 10),
+        height: animation.value,
+        width: animation.value,
+        child: const FlutterLogo(),
+      ),
+    );
+  }
+}
+
 class AnimationScreen extends StatefulWidget {
   const AnimationScreen({super.key});
 
@@ -17,22 +35,16 @@ class _AnimationScreenState extends State<AnimationScreen>
     super.initState();
     controller =
         AnimationController(duration: const Duration(seconds: 2), vsync: this);
-    animation = Tween<double>(begin: 0, end: 300).animate(controller)
-      ..addListener(() {
-        setState(() {});
-      });
+    animation = Tween<double>(begin: 0, end: 300).animate(controller);
     controller.forward();
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        margin: const EdgeInsets.symmetric(vertical: 10),
-        height: animation.value,
-        width: animation.value,
-        child: const FlutterLogo(),
-      ),
-    );
+  Widget build(BuildContext context) => AnimatedLogo(animation: animation);
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
   }
 }
