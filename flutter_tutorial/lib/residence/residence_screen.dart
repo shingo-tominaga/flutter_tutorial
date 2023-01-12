c09ef59270c07aaaa92ef2f53ddefa82405e9aeb
shingo.tominaga <shingo.tominaga@axia-works.com>
2023-01-12 19:30:36 +0900

    tutorial3_RoomInformationCardのStatelessWidget作成

--- flutter_tutorial/lib/residence/residence_screen.dart
+++ flutter_tutorial/lib/residence/residence_screen.dart
@@ -6,6 +6,7 @@ class ColorAssets {
   static const rgb229_235_234 = Color.fromARGB(255, 229, 235, 234);
   static const rgb228_99_79 = Color.fromARGB(255, 228, 99, 79);
   static const rgb249_246_242 = Color.fromARGB(255, 249, 246, 242);
+  static const rgb189_189_189 = Color.fromARGB(255, 189, 189, 189);
 }
 
 class ResidenceScreen extends StatefulWidget {
@@ -22,7 +23,11 @@ class _ResidenceScreenState extends State<ResidenceScreen> {
       home: Scaffold(
         appBar: const AppBarWidget(),
         body: ListView(
-          children: [_SearchConditionCard()],
+          children: [
+            _SearchConditionCard(),
+            _RoomInformationCard(),
+            _RoomInformationCard()
+          ],
         ),
       ),
     );
@@ -103,6 +108,8 @@ class _SearchConditionCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
+      shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(8))),
       child: Column(
         children: [
           Row(
@@ -159,10 +166,10 @@ class _SearchConditionCard extends StatelessWidget {
                     Icon(
                       Icons.train,
                       color: Colors.black54,
-                      size: 18,
+                      size: 16,
                     ),
                     SizedBox(
-                      width: 8,
+                      width: 4,
                     ),
                     Expanded(
                         child: Text(
@@ -185,8 +192,9 @@ class _SearchConditionCard extends StatelessWidget {
                     ),
                     // 本来はアイコン作ってもらったのを使うのでこういう調整とかはいらない
                     Container(
-                      width: 14,
-                      height: 14,
+                      width: 12,
+                      height: 12,
+                      alignment: Alignment.center,
                       decoration: const BoxDecoration(
                           color: Colors.black54, shape: BoxShape.circle),
                       child: const Icon(
@@ -196,7 +204,7 @@ class _SearchConditionCard extends StatelessWidget {
                       ),
                     ),
                     const SizedBox(
-                      width: 9,
+                      width: 6,
                     ),
                     const Text(
                       '下限なし 〜 2,000万円',
@@ -212,10 +220,10 @@ class _SearchConditionCard extends StatelessWidget {
                     Icon(
                       Icons.info_outline,
                       color: Colors.black54,
-                      size: 18,
+                      size: 16,
                     ),
                     SizedBox(
-                      width: 8,
+                      width: 4,
                     ),
                     Text(
                       '1R 〜 4LDK / 10㎡以上 / 徒歩20分',
@@ -223,6 +231,194 @@ class _SearchConditionCard extends StatelessWidget {
                     )
                   ],
                 )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _RoomInformationCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(8))),
+      clipBehavior: Clip.antiAliasWithSaveLayer,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Expanded(
+                flex: 1,
+                child: Image.asset(
+                  'images/img_no_image.png',
+                  fit: BoxFit.fitWidth,
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Image.asset(
+                  'images/img_floor_plan.png',
+                  fit: BoxFit.fitWidth,
+                ),
+              )
+            ],
+          ),
+          Container(
+            padding: const EdgeInsets.only(left: 16, top: 8),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Text(
+                  'Rising place川崎',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  '2,000万円',
+                  style: TextStyle(
+                      color: ColorAssets.rgb228_99_79,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.only(left: 14, top: 8),
+            child: Column(
+              children: [
+                Row(
+                  children: const [
+                    Icon(
+                      Icons.train,
+                      color: Colors.black54,
+                      size: 16,
+                    ),
+                    SizedBox(
+                      width: 4,
+                    ),
+                    Expanded(
+                        child: Text(
+                      '京急本線 京急川崎駅 より 徒歩9分',
+                      style: TextStyle(
+                        fontSize: 12,
+                      ),
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    ))
+                  ],
+                ),
+                const SizedBox(
+                  height: 4,
+                ),
+                Row(
+                  children: const [
+                    Icon(
+                      Icons.dashboard,
+                      color: Colors.black54,
+                      size: 16,
+                    ),
+                    SizedBox(
+                      width: 4,
+                    ),
+                    Expanded(
+                        child: Text(
+                      '1K / 21.24㎡ 南西向き',
+                      style: TextStyle(
+                        fontSize: 12,
+                      ),
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    ))
+                  ],
+                ),
+                const SizedBox(
+                  height: 4,
+                ),
+                Row(
+                  children: const [
+                    Icon(
+                      Icons.business,
+                      color: Colors.black54,
+                      size: 16,
+                    ),
+                    SizedBox(
+                      width: 4,
+                    ),
+                    Text(
+                      '2階/15階建 築5年',
+                      style: TextStyle(fontSize: 12),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            child: Row(
+              children: [
+                Expanded(
+                    flex: 1,
+                    child: TextButton(
+                      onPressed: null,
+                      child: Container(
+                        padding: const EdgeInsets.only(
+                            top: 8, bottom: 8, left: 16, right: 16),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(8),
+                            border: Border.all(
+                                width: 1.0, color: ColorAssets.rgb189_189_189)),
+                        child: Stack(
+                          children: [
+                            const Icon(
+                              Icons.delete,
+                              color: ColorAssets.rgb189_189_189,
+                            ),
+                            Container(
+                                alignment: Alignment.center,
+                                child: const Text(
+                                  '興味なし',
+                                  style: TextStyle(
+                                      color: Colors.black54, fontSize: 16),
+                                ))
+                          ],
+                        ),
+                      ),
+                    )),
+                Expanded(
+                    flex: 1,
+                    child: TextButton(
+                      onPressed: null,
+                      child: Container(
+                        width: double.infinity,
+                        padding: const EdgeInsets.only(
+                            top: 8, bottom: 8, left: 16, right: 16),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(8),
+                            border: Border.all(
+                                width: 1.0, color: ColorAssets.rgb189_189_189)),
+                        child: Stack(
+                          children: [
+                            const Icon(
+                              Icons.favorite_border,
+                              color: ColorAssets.rgb189_189_189,
+                            ),
+                            Container(
+                                alignment: Alignment.center,
+                                child: const Text(
+                                  'お気に入り',
+                                  style: TextStyle(
+                                      color: Colors.black54, fontSize: 16),
+                                ))
+                          ],
+                        ),
+                      ),
+                    ))
               ],
             ),
           )
